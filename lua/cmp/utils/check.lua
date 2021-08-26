e4a3f363891fb0234560d9c534df5e98f2a82c23
Zachary Churchill <zacharyachurchill@gmail.com>
2021-08-25 22:57:01 -0400

    add nofile bt to check.ok

--- lua/cmp/utils/check.lua
+++ lua/cmp/utils/check.lua
@@ -2,7 +2,9 @@ local check = {}
 
 check.ok = function()
   local ng = false
-  ng = ng or vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+  local bt = vim.api.nvim_buf_get_option(0, 'buftype')
+  ng = ng or bt == 'prompt'
+  ng = ng or bt == 'nofile'
   ng = ng or string.sub(vim.api.nvim_get_mode().mode, 1, 1) ~= 'i'
   return not ng
 end
