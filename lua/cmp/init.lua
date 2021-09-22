38ad2e371c9d971650264a03905731a743c6f077
hrsh7th <hrsh7th@gmail.com>
2021-09-22 13:57:43 +0900

    refactor

--- lua/cmp/init.lua
+++ lua/cmp/init.lua
@@ -75,9 +75,8 @@ end
 ---Select next item if possible
 cmp.select_next_item = function()
   if cmp.core.view:visible() then
-    local release = cmp.core:suspend()
     cmp.core.view:select_next_item()
-    vim.schedule(release)
+    cmp.core:get_context()
     return true
   else
     return false
@@ -87,9 +86,8 @@ end
 ---Select prev item if possible
 cmp.select_prev_item = function()
   if cmp.core.view:visible() then
-    local release = cmp.core:suspend()
     cmp.core.view:select_prev_item()
-    vim.schedule(release)
+    cmp.core:get_context()
     return true
   else
     return false
