d561c60a7552fa300f3997d331a4b03b427073b5
hrsh7th <hrsh7th@gmail.com>
2021-09-23 18:44:20 +0900

    extmark ephemeral ghost text

--- lua/cmp/view/ghost_text_view.lua
+++ lua/cmp/view/ghost_text_view.lua
@@ -9,47 +9,58 @@ ghost_text_view.ns = vim.api.nvim_create_namespace('cmp:GHOST_TEXT')
 
 ghost_text_view.new = function()
   local self = setmetatable({}, { __index = ghost_text_view })
+  self.win = nil
+  self.entry = nil
+  vim.api.nvim_set_decoration_provider(ghost_text_view.ns, {
+    on_win = function(_, win)
+      return win == self.win
+    end,
+    on_line = function()
+      local c = config.get().experimental.ghost_text
+      if not c then
+        return
+      end
+
+      if not self.entry then
+        return
+      end
+
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      if string.sub(vim.api.nvim_get_current_line(), cursor[2] + 1) ~= '' then
+        return
+      end
+
+      local diff = 1 + cursor[2] - self.entry:get_offset()
+      local text = self.entry:get_insert_text()
+      if self.entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
+        text = vim.lsp.util.parse_snippet(text)
+      end
+      text = string.sub(str.oneline(text), diff + 1)
+      if #text > 0 then
+        vim.api.nvim_buf_set_extmark(0, ghost_text_view.ns, cursor[1] - 1, cursor[2], {
+          right_gravity = false,
+          virt_text = { { text, c.hl_group or 'Comment' } },
+          virt_text_pos = 'overlay',
+          hl_mode = 'combine',
+          ephemeral = true,
+        })
+      end
+    end
+  })
   return self
 end
 
 ---Show ghost text
 ---@param e cmp.Entry
-ghost_text_view.show = function(_, e)
-  vim.api.nvim_buf_clear_namespace(0, ghost_text_view.ns, 0, -1)
-
-  local c = config.get().experimental.ghost_text
-  if not c then
-    return
-  end
-
-  if not e then
-    return
-  end
-
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  if string.sub(vim.api.nvim_get_current_line(), cursor[2] + 1) ~= '' then
-    return
-  end
-
-  local diff = 1 + cursor[2] - e:get_offset()
-  local text = e:get_insert_text()
-  if e.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
-    text = vim.lsp.util.parse_snippet(text)
-  end
-  text = string.sub(str.oneline(text), diff + 1)
-  if #text > 0 then
-    vim.api.nvim_buf_set_extmark(0, ghost_text_view.ns, cursor[1] - 1, cursor[2], {
-      right_gravity = false,
-      virt_text = { { text, c.hl_group or 'Comment' } },
-      virt_text_pos = 'overlay',
-      hl_mode = 'combine',
-      priority = 1,
-    })
-  end
+ghost_text_view.show = function(self, e)
+  self.win = vim.api.nvim_get_current_win()
+  self.entry = e
 end
 
-ghost_text_view.hide = function()
-  vim.api.nvim_buf_clear_namespace(0, ghost_text_view.ns, 0, -1)
+ghost_text_view.hide = function(self)
+  self.win = nil
+  self.entry = nil
 end
 
 return ghost_text_view
+
