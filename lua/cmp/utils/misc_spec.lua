8830f608d81ccd6bc58f12c02a72054be944be69
Magnus Bergmark <me@mange.dev>
2021-10-13 16:27:10 +0200

    Cover utils.misc.merge with more test cases

--- lua/cmp/utils/misc_spec.lua
+++ lua/cmp/utils/misc_spec.lua
@@ -5,23 +5,66 @@ local misc = require('cmp.utils.misc')
 describe('misc', function()
   before_each(spec.before)
 
-  it('merge', function()
-    local merged
-    merged = misc.merge({
-      a = {},
-    }, {
-      a = {
-        b = 1,
-      },
-    })
-    assert.are.equal(merged.a.b, 1)
-    merged = misc.merge({
-      a = false,
-    }, {
-      a = {
-        b = 1,
-      },
-    })
-    assert.are.equal(merged.a, false)
+  describe('merge', function()
+    it('replaces values from the left', function()
+      local merged = misc.merge({
+        recipient = 'World',
+        new_key = 'I am new',
+      }, {
+        greeting = 'Hello',
+        recipient = 'Handsome',
+      })
+      assert.are.equal(merged.greeting, 'Hello')
+      assert.are.equal(merged.recipient, 'World')
+      assert.are.equal(merged.new_key, 'I am new')
+    end)
+
+    it('merges subkeys of tables from the left', function()
+      local merged = misc.merge({
+        x = {
+          c = 12,
+        },
+      }, {
+        x = {
+          a = 1,
+          b = 2,
+          c = 3,
+        },
+      })
+      assert.are.same(merged.x, { a = 1, b = 2, c = 12 })
+    end)
+
+    it('does not change tables when merging empty table', function()
+      local merged = misc.merge({
+        tbl = {},
+        other = {},
+      }, {
+        tbl = { hello = 'world' },
+        other = 'hello',
+      })
+      assert.are.same(merged, {
+        tbl = { hello = 'world' },
+        other = {},
+      })
+    end)
+
+    it('replaces non-tables types', function()
+      local merged
+      merged = misc.merge({
+        arr = { 'hello' },
+        num = 42,
+        boo = false,
+        str = 'world',
+      }, {
+        arr = { 'see ya' },
+        num = 41,
+        boo = true,
+        str = 'land',
+      })
+      assert.are.same(merged.arr, { 'hello' })
+      assert.are.equal(merged.num, 42)
+      assert.are.equal(merged.boo, false)
+      assert.are.equal(merged.str, 'world')
+    end)
   end)
 end)
