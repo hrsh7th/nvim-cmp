e5ae50d5454780c902857123a4446069be250cd2
windwp <baronnight19@gmail.com>
2021-11-01 08:08:27 +0700

    feat: custom highlight on kind item #327
    ```lua
    --sample
    
    local lsp_kinds = {
        Text                  = {''  , 'TSText'        } ,
        Method                = {'ƒ'  , 'TSMethod'      } ,
        Function              = {'ƒ'  , 'TSFunction'    } ,
        Constructor           = {''  , 'TSConstructor' } ,
        Field                 = {'⌘'  , 'TSField'       } ,
        Variable              = {''  , 'TSVariable'    } ,
        Class                 = {''  , 'TSConstructor' } ,
        Interface             = {''  , 'TSAnnotation'  } ,
        Module                = {''  , 'TSConstant'    } ,
        Property              = {''  , 'TSProperty'    } ,
        Unit                  = {''  , 'TSTag'         } ,
        Value                 = {''  , 'TSValue'       } ,
        Enum                  = {'了' , 'TSMethod'      } ,
        Keyword               = {''  , 'TSKeyword'     } ,
        Snippet               = {'﬌'  , 'TSLabel'       } ,
        Color                 = {''  , 'TSBoolean'     } ,
        File                  = {''  , 'TSSymbol'      } ,
        Reference             = {''  , 'TSReference'   } ,
        Folder                = {''  , 'Directory'     } ,
        EnumMember            = {''  , 'TSPunctSpecial'} ,
        Constant              = {''  , 'TSConstant'    } ,
        Struct                = {''  , 'TSStruct'      } ,
        Event                 = {''  , 'TSEvent'       } ,
        Operator              = {''  , 'TSOperator'    } ,
        TypeParameter         = {''  , 'TSType'        } ,
    }
    
    cmp.setup({
        formatting={
          format = function(_, vim_item)
            vim_item.kind_highlight = lsp_kinds[vim_item.kind][2]
            vim_item.kind = lsp_kinds[vim_item.kind][1]
            return vim_item
          end,
        }
    })
    ```

--- lua/cmp/types/vim.lua
+++ lua/cmp/types/vim.lua
@@ -2,6 +2,7 @@
 ---@field public word string
 ---@field public abbr string|nil
 ---@field public kind string|nil
+---@field public kind_highlight string|nil
 ---@field public menu string|nil
 ---@field public equal "1"|nil
 ---@field public empty "1"|nil
