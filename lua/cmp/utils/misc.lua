dfe41c9b7e40436ed9dc30f6584f31799744d05d
max397574 <max.heinzer397@gmx.ch>
2021-12-28 17:39:55 +0100

    feat(utils): added function to check if in comment

--- lua/cmp/utils/misc.lua
+++ lua/cmp/utils/misc.lua
@@ -14,6 +14,67 @@ misc.once = function(callback)
   end
 end
 
+---Checks if cursor is inside a comment
+---@return boolean
+misc.is_inside_comment = function()
+  local highlighter = require('vim.treesitter.highlighter')
+  local ts_utils = require('nvim-treesitter.ts_utils')
+  local buf = vim.api.nvim_get_current_buf()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  row = row - 1
+  if vim.api.nvim_get_mode().mode == 'i' then
+    col = col - 1
+  end
+
+  local self = highlighter.active[buf]
+  if not self then
+    return {}
+  end
+
+  local matches = {}
+
+  self.tree:for_each_tree(function(tstree, tree)
+    if not tstree then
+      return
+    end
+
+    local root = tstree:root()
+    local root_start_row, _, root_end_row, _ = root:range()
+
+    if root_start_row > row or root_end_row < row then
+      return
+    end
+
+    local query = self:get_query(tree:lang())
+
+    if not query:query() then
+      return
+    end
+
+    local iter = query:query():iter_captures(root, self.bufnr, row, row + 1)
+
+    for capture, node, _ in iter do
+      local hl = query.hl_cache[capture]
+
+      if hl and ts_utils.is_in_node_range(node, row, col) then
+        local c = query._query.captures[capture] -- name of the capture in the query
+        if c ~= nil then
+          local general_hl = query:_get_hl_from_capture(capture)
+          table.insert(matches, general_hl)
+        end
+      end
+    end
+  end, true)
+  local lnum, col = vim.fn.line('.'), math.min(vim.fn.col('.'), #vim.fn.getline('.'))
+  for _, syn_id in ipairs(vim.fn.synstack(lnum, col)) do
+    syn_id = vim.fn.synIDtrans(syn_id) -- Resolve :highlight links
+    if vim.fn.synIDattr(syn_id, 'name') == 'Comment' then
+      return true
+    end
+  end
+  return vim.tbl_contains(matches, 'TSComment')
+end
+
 ---Return concatenated list
 ---@param list1 any[]
 ---@param list2 any[]
