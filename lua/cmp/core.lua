f7988559905d8f2866b142c056f47d828f086e8a
hrsh7th <hrsh7th@gmail.com>
2022-01-13 22:48:11 +0900

    Fix offset encoding enum

--- lua/cmp/core.lua
+++ lua/cmp/core.lua
@@ -366,10 +366,10 @@ core.confirm = function(self, e, option, callback)
         if has_cursor_line_text_edit then
           return
         end
-        vim.lsp.util.apply_text_edits(text_edits, ctx.bufnr, 'utf8')
+        vim.lsp.util.apply_text_edits(text_edits, ctx.bufnr, 'utf-8')
       end)
     else
-      vim.lsp.util.apply_text_edits(e:get_completion_item().additionalTextEdits, ctx.bufnr, 'utf8')
+      vim.lsp.util.apply_text_edits(e:get_completion_item().additionalTextEdits, ctx.bufnr, 'utf-8')
     end
   end)
   feedkeys.call('', 'n', function()
@@ -399,7 +399,7 @@ core.confirm = function(self, e, option, callback)
       if is_snippet then
         completion_item.textEdit.newText = ''
       end
-      vim.lsp.util.apply_text_edits({ completion_item.textEdit }, ctx.bufnr, 'utf8')
+      vim.lsp.util.apply_text_edits({ completion_item.textEdit }, ctx.bufnr, 'utf-8')
       local texts = vim.split(completion_item.textEdit.newText, '\n')
       local position = completion_item.textEdit.range.start
       position.line = position.line + (#texts - 1)
