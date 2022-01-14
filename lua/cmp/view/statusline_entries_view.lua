7a533dcf68f5a7f3bdcf3b1b7e0f5ece4a8e76b5
Nir Tzachar <nir.tzachar@gmail.com>
2022-01-14 23:03:57 +0200

    move to a floating window instead of abusing status line

--- lua/cmp/view/statusline_entries_view.lua
+++ lua/cmp/view/statusline_entries_view.lua
@@ -2,6 +2,7 @@ local event = require('cmp.utils.event')
 local autocmd = require('cmp.utils.autocmd')
 local feedkeys = require('cmp.utils.feedkeys')
 local config = require('cmp.config')
+local window = require('cmp.utils.window')
 local types = require('cmp.types')
 local keymap = require('cmp.utils.keymap')
 local misc = require('cmp.utils.misc')
@@ -9,6 +10,7 @@ local api = require('cmp.utils.api')
 
 ---@class cmp.CustomEntriesView
 ---@field private offset number
+---@field private entries_win cmp.Window
 ---@field private active boolean
 ---@field private entries cmp.Entry[]
 ---@field public event cmp.Event
@@ -21,7 +23,6 @@ statusline_entries_view.init = function(self)
   self.offset = -1
   self.active = false
   self.entries = {}
-  self.org_statusline = nil
   self.selected_index = nil
   self.last_displayed_indices = {}
   self.moving_forwards = nil
@@ -30,6 +31,62 @@ end
 statusline_entries_view.new = function()
   local self = setmetatable({}, { __index = statusline_entries_view })
   self:init()
+
+  self.entries_win = window.new()
+  self.entries_win:option('conceallevel', 2)
+  self.entries_win:option('concealcursor', 'n')
+  self.entries_win:option('cursorlineopt', 'line')
+  self.entries_win:option('foldenable', false)
+  self.entries_win:option('wrap', false)
+  self.entries_win:option('scrolloff', 0)
+  self.entries_win:option('winhighlight', 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None')
+  self.entries_win:buffer_option('tabstop', 1)
+
+  vim.api.nvim_set_decoration_provider(statusline_entries_view.ns, {
+    on_win = function(_, win, buf, _, _)
+      if win ~= self.entries_win.win or buf ~= self.entries_win:get_buffer() then
+        return
+      end
+
+      dump('========================================')
+      local location = 0
+      for _, i in ipairs(self.last_displayed_indices) do
+        local e = self.entries[i]
+        if e then
+          local view = e:get_view(self.offset, buf)
+          vim.api.nvim_buf_set_extmark(buf, statusline_entries_view.ns, 0, location, {
+            end_line = 0,
+            end_col = location + view['abbr'].bytes,
+            hl_group = view['abbr'].hl_group,
+            hl_mode = 'combine',
+            ephemeral = true,
+          })
+
+          if i == self.selected_index then
+            vim.api.nvim_buf_set_extmark(buf, statusline_entries_view.ns, 0, location, {
+              end_line = 0,
+              end_col = location + view['abbr'].bytes,
+              hl_group = 'PmenuSel',
+              hl_mode = 'combine',
+              ephemeral = true,
+            })
+          end
+
+          for _, m in ipairs(e.matches or {}) do
+            vim.api.nvim_buf_set_extmark(buf, statusline_entries_view.ns, 0, location + m.word_match_start - 1, {
+              end_line = 0,
+              end_col = location + m.word_match_end,
+              hl_group = m.fuzzy and 'CmpItemAbbrMatchFuzzy' or 'CmpItemAbbrMatch',
+              hl_mode = 'combine',
+              ephemeral = true,
+            })
+          end
+
+          location = location + view['abbr'].bytes + 1
+        end
+      end
+    end,
+  })
 
   autocmd.subscribe(
     'CompleteChanged',
@@ -43,9 +100,7 @@ statusline_entries_view.new = function()
 end
 
 statusline_entries_view.close = function(self)
-  if self.org_statusline ~= nil then
-    vim.o.statusline = self.org_statusline
-  end
+  self.entries_win:close()
   self:init()
 end
 
@@ -58,14 +113,14 @@ statusline_entries_view.on_change = function(self)
 end
 
 statusline_entries_view.open = function(self, offset, entries)
-  if self.org_statusline == nil then
-    self.org_statusline = vim.o.statusline
-  end
   self.offset = offset
   self.entries = {}
   self.last_displayed_indices = {}
 
-  local lines = {}
+  -- Apply window options (that might be changed) on the custom completion menu.
+  self.entries_win:option('winblend', vim.o.pumblend)
+
+  -- local entries_buf = self.entries_win:get_buffer()
   local dedup = {}
   local preselect = 0
   local i = 1
@@ -74,7 +129,6 @@ statusline_entries_view.open = function(self, offset, entries)
     if view.dup == 1 or not dedup[e.completion_item.label] then
       dedup[e.completion_item.label] = true
       table.insert(self.entries, e)
-      table.insert(lines, ' ')
       if preselect == 0 and e.completion_item.preselect then
         preselect = i
       end
@@ -82,6 +136,15 @@ statusline_entries_view.open = function(self, offset, entries)
     end
   end
 
+  self.entries_win:open({
+    relative = 'editor',
+    style = 'minimal',
+    row = vim.api.nvim_win_get_height(0),
+    col = 0,
+    width = vim.api.nvim_win_get_width(0),
+    height = 1,
+    zindex = 1001,
+  })
   if preselect > 0 and config.get().preselect == types.cmp.PreselectMode.Item then
     self:_select(preselect, { behavior = types.cmp.SelectBehavior.Select })
   elseif not string.match(config.get().completion.completeopt, 'noselect') then
@@ -96,47 +159,20 @@ statusline_entries_view.abort = function(self)
     self:close()
   end)
 end
--- format entry for status line.
--- only use the 'abbr' field
--- return formatted text, original text, view length
-statusline_entries_view.highlight_entry = function(self, entry)
-  local view = entry:get_view(self.offset, 0)
-  local word = view['abbr'].text
-  local view_length = view['abbr'].bytes
-  local hl_group = '%#' .. view['abbr'].hl_group .. '#'
-  local fuzzy_group = '%#' .. 'CmpItemAbbrMatchFuzzy' .. '#'
-  local match_group = '%#' .. 'CmpItemAbbrMatch' .. '#'
-  local text = { hl_group }
-  local last_offset = 1
-  for _, m in ipairs(entry.matches or {}) do
-    if m.word_match_start > last_offset then
-      -- add everything b4 m.word_match_start
-      table.insert(text, word:sub(last_offset, m.word_match_start - 1))
-    end
-    table.insert(text, m.fuzzy and fuzzy_group or match_group)
-    table.insert(text, word:sub(m.word_match_start, m.word_match_end))
-    table.insert(text, hl_group)
-    last_offset = m.word_match_end + 1
-  end
-  table.insert(text, '%#Normal#')
-  table.insert(text, word:sub(last_offset))
-
-  return table.concat(text, ''), word, view_length
-end
 
 statusline_entries_view.draw = function(self)
+  local entries_buf = self.entries_win:get_buffer()
   local texts = {}
   local lengths = {}
   for i, e in ipairs(self.entries) do
     if e then
-      local formatted_text, text, view_length = self:highlight_entry(e)
-      if i == self.selected_index then
-        formatted_text = '%#CursorLine#' .. text .. '%#Normal#'
-      end
-      table.insert(lengths, view_length)
-      table.insert(texts, formatted_text)
+      local view = e:get_view(self.offset, entries_buf)
+      -- add 1 to lengths, to account for the added separator
+      table.insert(lengths, view['abbr'].bytes + 1)
+      table.insert(texts, view['abbr'].text)
     end
   end
+
   local selected_index = (self.selected_index or 1)
   local start_index = (self.selected_index or 1)
   local lst_dspl_ind = self.last_displayed_indices
@@ -144,7 +180,6 @@ statusline_entries_view.draw = function(self)
     start_index = start_index
   elseif vim.tbl_contains(lst_dspl_ind, selected_index) then
     start_index = lst_dspl_ind[1]
-    -- elseif start_index > lst_dspl_ind[#lst_dspl_ind] then
   elseif self.moving_forwards then
     local needed_length = lengths[selected_index]
     start_index = lst_dspl_ind[1]
@@ -170,22 +205,25 @@ statusline_entries_view.draw = function(self)
     if index > #self.entries then
       index = index - #self.entries
     end
-    if total_length + lengths[index] < vim.api.nvim_win_get_width(0) then
+    if total_length + lengths[index] < vim.api.nvim_win_get_width(self.entries_win.win) then
       if total_length ~= 0 and index == start_index then
         break
       end
       table.insert(statusline, texts[index])
       table.insert(displayed_indices, index)
-      -- add 1 for the space between items
-      total_length = total_length + lengths[index] + 1
+      total_length = total_length + lengths[index]
     else
+      -- always add the last entry
+      table.insert(statusline, texts[index])
       break
     end
   end
 
   statusline = table.concat(statusline, '|')
   self.last_displayed_indices = displayed_indices
-  vim.o.statusline = statusline
+
+  vim.api.nvim_buf_set_lines(entries_buf, 0, 1, false, { statusline })
+  vim.api.nvim_buf_set_option(entries_buf, 'modified', false)
 
   vim.api.nvim_win_call(0, function()
     misc.redraw()
@@ -193,11 +231,11 @@ statusline_entries_view.draw = function(self)
 end
 
 statusline_entries_view.visible = function(self)
-  return self.org_statusline ~= nil
+  return self.entries_win:visible()
 end
 
 statusline_entries_view.info = function(self)
-  return nil
+  return self.entries_win:info()
 end
 
 statusline_entries_view.select_next_item = function(self, option)
@@ -251,6 +289,7 @@ statusline_entries_view._select = function(self, selected_index, option)
     vim.api.nvim_feedkeys(keymap.backspace(length) .. word, 'int', true)
   end
 
+  self.entries_win:update()
   self:draw()
   self.event:emit('change')
 end
