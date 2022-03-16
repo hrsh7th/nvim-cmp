ebb67c0338aab268b1aa3ea204a3fa3eb145a0a8
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-16 15:25:42 +0800

    Implement vim.api.nvim_create_autocmd

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -3,56 +3,46 @@ if vim.g.loaded_cmp then
 end
 vim.g.loaded_cmp = true
 
-local api = require "cmp.utils.api"
+local api = require('cmp.utils.api')
 local misc = require('cmp.utils.misc')
 local types = require('cmp.types')
 local config = require('cmp.config')
 local highlight = require('cmp.utils.highlight')
+local emit = require('cmp.utils.autocmd').emit
 
--- TODO: https://github.com/neovim/neovim/pull/14661
-vim.cmd [[
-  augroup ___cmp___
-    autocmd!
-    autocmd InsertEnter * lua require'cmp.utils.autocmd'.emit('InsertEnter')
-    autocmd InsertLeave * lua require'cmp.utils.autocmd'.emit('InsertLeave')
-    autocmd TextChangedI,TextChangedP * lua require'cmp.utils.autocmd'.emit('TextChanged')
-    autocmd CursorMovedI * lua require'cmp.utils.autocmd'.emit('CursorMoved')
-    autocmd CompleteChanged * lua require'cmp.utils.autocmd'.emit('CompleteChanged')
-    autocmd CompleteDone * lua require'cmp.utils.autocmd'.emit('CompleteDone')
-    autocmd ColorScheme * call v:lua.cmp.plugin.colorscheme()
-    autocmd CmdlineEnter * call v:lua.cmp.plugin.cmdline.enter()
-    autocmd CmdwinEnter * call v:lua.cmp.plugin.cmdline.leave() " for entering cmdwin with `<C-f>`
-  augroup END
-]]
+misc.set(_G, { 'cmp', 'plugin', 'cmdline', 'leave' }, function()
+  if vim.fn.expand('<afile>') ~= '=' then
+    vim.cmd([[
+      augroup cmp-cmdline
+        autocmd!
+      augroup END
+    ]])
+    emit('CmdlineLeave')
+  end
+end)
 
 misc.set(_G, { 'cmp', 'plugin', 'cmdline', 'enter' }, function()
   if config.is_native_menu() then
     return
   end
-  if vim.fn.expand('<afile>')~= '=' then
+  if vim.fn.expand('<afile>') ~= '=' then
     vim.schedule(function()
       if api.is_cmdline_mode() then
-        vim.cmd [[
-          augroup cmp-cmdline
-            autocmd!
-            autocmd CmdlineChanged * lua require'cmp.utils.autocmd'.emit('TextChanged')
-            autocmd CmdlineLeave * call v:lua.cmp.plugin.cmdline.leave()
-          augroup END
-        ]]
-        require('cmp.utils.autocmd').emit('CmdlineEnter')
+        local cmp_cmdline = vim.api.nvim_create_augroup('cmp_cmdline', { clear = true })
+        vim.api.nvim_create_autocmd('CmdlineChanged', {
+          group = cmp_cmdline,
+          callback = function()
+            emit('TextChanged')
+          end,
+        })
+
+        vim.api.nvim_create_autocmd('CmdlineLeave', {
+          group = cmp_cmdline,
+          callback = _G.cmp.plugin.cmdline.leave,
+        })
+        emit('CmdlineEnter')
       end
     end)
-  end
-end)
-
-misc.set(_G, { 'cmp', 'plugin', 'cmdline', 'leave' }, function()
-  if vim.fn.expand('<afile>') ~= '=' then
-    vim.cmd [[
-      augroup cmp-cmdline
-        autocmd!
-      augroup END
-    ]]
-    require('cmp.utils.autocmd').emit('CmdlineLeave')
   end
 end)
 
@@ -93,23 +83,23 @@ end)
 _G.cmp.plugin.colorscheme()
 
 if vim.fn.hlexists('CmpItemAbbr') ~= 1 then
-  vim.cmd [[highlight default link CmpItemAbbr CmpItemAbbrDefault]]
+  vim.cmd([[highlight default link CmpItemAbbr CmpItemAbbrDefault]])
 end
 
 if vim.fn.hlexists('CmpItemAbbrDeprecated') ~= 1 then
-  vim.cmd [[highlight default link CmpItemAbbrDeprecated CmpItemAbbrDeprecatedDefault]]
+  vim.cmd([[highlight default link CmpItemAbbrDeprecated CmpItemAbbrDeprecatedDefault]])
 end
 
 if vim.fn.hlexists('CmpItemAbbrMatch') ~= 1 then
-  vim.cmd [[highlight default link CmpItemAbbrMatch CmpItemAbbrMatchDefault]]
+  vim.cmd([[highlight default link CmpItemAbbrMatch CmpItemAbbrMatchDefault]])
 end
 
 if vim.fn.hlexists('CmpItemAbbrMatchFuzzy') ~= 1 then
-  vim.cmd [[highlight default link CmpItemAbbrMatchFuzzy CmpItemAbbrMatchFuzzyDefault]]
+  vim.cmd([[highlight default link CmpItemAbbrMatchFuzzy CmpItemAbbrMatchFuzzyDefault]])
 end
 
 if vim.fn.hlexists('CmpItemKind') ~= 1 then
-  vim.cmd [[highlight default link CmpItemKind CmpItemKindDefault]]
+  vim.cmd([[highlight default link CmpItemKind CmpItemKindDefault]])
 end
 for name in pairs(types.lsp.CompletionItemKind) do
   if type(name) == 'string' then
@@ -121,12 +111,12 @@ for name in pairs(types.lsp.CompletionItemKind) do
 end
 
 if vim.fn.hlexists('CmpItemMenu') ~= 1 then
-  vim.cmd [[highlight default link CmpItemMenu CmpItemMenuDefault]]
+  vim.cmd([[highlight default link CmpItemMenu CmpItemMenuDefault]])
 end
 
-vim.cmd [[command! CmpStatus lua require('cmp').status()]]
+vim.cmd([[command! CmpStatus lua require('cmp').status()]])
 
-vim.cmd [[doautocmd <nomodeline> User CmpReady]]
+vim.cmd([[doautocmd <nomodeline> User CmpReady]])
 
 if vim.on_key then
   vim.on_key(function(keys)
@@ -140,3 +130,66 @@ if vim.on_key then
   end, vim.api.nvim_create_namespace('cmp.plugin'))
 end
 
+local ___cmp___ = vim.api.nvim_create_augroup('___cmp___', { clear = true })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = ___cmp___,
+  callback = function()
+    emit('InsertEnter')
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = ___cmp___,
+  callback = function()
+    emit('InsertLeave')
+  end,
+})
+
+vim.api.nvim_create_autocmd({
+  'TextChangedI',
+  'TextChangedP',
+}, {
+
+  group = ___cmp___,
+  callback = function()
+    emit('TextChanged')
+  end,
+})
+
+vim.api.nvim_create_autocmd('CursorMovedI', {
+  group = ___cmp___,
+  callback = function()
+    emit('CursorMoved')
+  end,
+})
+
+vim.api.nvim_create_autocmd('CompleteChanged', {
+  group = ___cmp___,
+  callback = function()
+    emit('CompleteChanged')
+  end,
+})
+
+vim.api.nvim_create_autocmd('CompleteDone', {
+  group = ___cmp___,
+  callback = function()
+    emit('CompleteDone')
+  end,
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = ___cmp___,
+  callback = _G.cmp.plugin.colorscheme,
+})
+
+-- for entering cmdwin with `<C-f>`
+vim.api.nvim_create_autocmd('CmdlineEnter', {
+  group = ___cmp___,
+  callback = _G.cmp.plugin.cmdline.enter,
+})
+
+-- -    autocmd CmdlineEnter * call v:lua.cmp.plugin.cmdline.enter()
+vim.api.nvim_create_autocmd('CmdwinEnter', {
+  group = ___cmp___,
+  callback = _G.cmp.plugin.cmdline.leave,
+})
