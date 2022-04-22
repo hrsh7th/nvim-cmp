9e1962bd3b85c3d8a7744d0cbd6c1fa77823da89
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-04-22 08:21:37 +0800

    Add descriptions on autocmds

--- lua/cmp/autocmds.lua
+++ lua/cmp/autocmds.lua
@@ -8,6 +8,7 @@ function M.autocmd()
     callback = function()
       emit('InsertEnter')
     end,
+    desc = [[autocmd InsertEnter * lua require'cmp.utils.autocmd'.emit('InsertEnter')]],
   })
 
   vim.api.nvim_create_autocmd('InsertLeave', {
@@ -15,6 +16,7 @@ function M.autocmd()
     callback = function()
       emit('InsertLeave')
     end,
+    desc = [[autocmd InsertLeave * lua require'cmp.utils.autocmd'.emit('InsertLeave')]],
   })
 
   vim.api.nvim_create_autocmd({
@@ -26,6 +28,7 @@ function M.autocmd()
     callback = function()
       emit('TextChanged')
     end,
+    desc = [[autocmd TextChangedI,TextChangedP * lua require'cmp.utils.autocmd'.emit('TextChanged')]],
   })
 
   vim.api.nvim_create_autocmd('CursorMovedI', {
@@ -33,6 +36,7 @@ function M.autocmd()
     callback = function()
       emit('CursorMoved')
     end,
+    desc = [[autocmd CursorMovedI * lua require'cmp.utils.autocmd'.emit('CursorMoved')]],
   })
 
   vim.api.nvim_create_autocmd('CompleteChanged', {
@@ -40,6 +44,7 @@ function M.autocmd()
     callback = function()
       emit('CompleteChanged')
     end,
+    desc = [[autocmd CompleteChanged * lua require'cmp.utils.autocmd'.emit('CompleteChanged')]],
   })
 
   vim.api.nvim_create_autocmd('CompleteDone', {
@@ -47,22 +52,26 @@ function M.autocmd()
     callback = function()
       emit('CompleteDone')
     end,
+    desc = [[autocmd CompleteDone * lua require'cmp.utils.autocmd'.emit('CompleteDone')]],
   })
 
   vim.api.nvim_create_autocmd('ColorScheme', {
     group = ___cmp___,
     callback = _G.cmp.plugin.colorscheme,
+    desc = [[autocmd ColorScheme * call v:lua.cmp.plugin.colorscheme()]],
   })
 
   -- for entering cmdwin with `<C-f>`
   vim.api.nvim_create_autocmd('CmdlineEnter', {
     group = ___cmp___,
     callback = _G.cmp.plugin.cmdline.enter,
+    desc = [[autocmd CmdlineEnter * call v:lua.cmp.plugin.cmdline.enter()]],
   })
 
   vim.api.nvim_create_autocmd('CmdwinEnter', {
     group = ___cmp___,
     callback = _G.cmp.plugin.cmdline.leave,
+    desc = [[autocmd CmdwinEnter * call v:lua.cmp.plugin.cmdline.leave()]],
   })
 end
 
@@ -73,11 +82,13 @@ function M.cmdline_mode()
     callback = function()
       emit('TextChanged')
     end,
+    desc = [[autocmd CmdlineChanged * lua require'cmp.utils.autocmd'.emit('TextChanged')]],
   })
 
   vim.api.nvim_create_autocmd('CmdlineLeave', {
     group = cmp_cmdline,
     callback = _G.cmp.plugin.cmdline.leave,
+    desc = [[autocmd CmdlineLeave * call v:lua.cmp.plugin.cmdline.leave()]],
   })
 end
 
