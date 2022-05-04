ef11be792265e1228553e1576443da002376b106
nate maile <npmaile@gmail.com>
2022-05-03 22:25:11 -0400

    fix my fix

--- lua/cmp/utils/highlight.lua
+++ lua/cmp/utils/highlight.lua
@@ -20,7 +20,7 @@ highlight.inherit = function(name, source, settings)
         v = v == 1
       end
       if v then
-        settings[key] = v == '' and 'NONE' or v
+        settings[key] = v == '' or 'NONE' or v
       end
     end
   end
