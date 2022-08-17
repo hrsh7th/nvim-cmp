hrsh7th/nvim-cmp

commit 8ce0832a5c409644fa06f34337b74f11aaa871c7

The default highlight groups shouldn't be default=true. Fixes #972

plugin/cmp.lua
@@ -26,15 +26,15 @@ for kind in pairs(types.lsp.CompletionItemKind) do
 end
 
 autocmd.subscribe('ColorScheme', function()
-  highlight.inherit('CmpItemAbbrDefault', 'Pmenu', { bg = 'NONE', default = true })
-  highlight.inherit('CmpItemAbbrDeprecatedDefault', 'Comment', { bg = 'NONE', default = true })
-  highlight.inherit('CmpItemAbbrMatchDefault', 'Pmenu', { bg = 'NONE', default = true })
-  highlight.inherit('CmpItemAbbrMatchFuzzyDefault', 'Pmenu', { bg = 'NONE', default = true })
-  highlight.inherit('CmpItemKindDefault', 'Special', { bg = 'NONE', default = true })
-  highlight.inherit('CmpItemMenuDefault', 'Pmenu', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemAbbrDefault', 'Pmenu', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemAbbrDeprecatedDefault', 'Comment', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemAbbrMatchDefault', 'Pmenu', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemAbbrMatchFuzzyDefault', 'Pmenu', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemKindDefault', 'Special', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemMenuDefault', 'Pmenu', { bg = 'NONE', default = false })
   for name in pairs(types.lsp.CompletionItemKind) do
     if type(name) == 'string' then
-      vim.api.nvim_set_hl(0, ('CmpItemKind%sDefault'):format(name), { link = 'CmpItemKind', default = true })
+      vim.api.nvim_set_hl(0, ('CmpItemKind%sDefault'):format(name), { link = 'CmpItemKind', default = false })
     end
   end
 end)

commit 8c2feb399135df153b581895b267d4dfd37ad223

fix(plugin): overriding colorschemes
Commit 8ce0832a5c409644fa06f34337b74f11aaa871c7 broke my colorscheme and
other colorschemes which provide nvim-cmp highlights (e.g.
linking `CmpItemKindText` to `Text` doesn't work). Plugins in general
should definitely use `default = true` to maintain compatability with
colorschemes, otherwise they will clobber the groups set by the
colorscheme (which is what is happening now).

plugin/cmp.lua
@@ -26,15 +26,15 @@ for kind in pairs(types.lsp.CompletionItemKind) do
 end
 
 autocmd.subscribe('ColorScheme', function()
-  highlight.inherit('CmpItemAbbrDefault', 'Pmenu', { bg = 'NONE', default = false })
-  highlight.inherit('CmpItemAbbrDeprecatedDefault', 'Comment', { bg = 'NONE', default = false })
-  highlight.inherit('CmpItemAbbrMatchDefault', 'Pmenu', { bg = 'NONE', default = false })
-  highlight.inherit('CmpItemAbbrMatchFuzzyDefault', 'Pmenu', { bg = 'NONE', default = false })
-  highlight.inherit('CmpItemKindDefault', 'Special', { bg = 'NONE', default = false })
-  highlight.inherit('CmpItemMenuDefault', 'Pmenu', { bg = 'NONE', default = false })
+  highlight.inherit('CmpItemAbbrDefault', 'Pmenu', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemAbbrDeprecatedDefault', 'Comment', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemAbbrMatchDefault', 'Pmenu', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemAbbrMatchFuzzyDefault', 'Pmenu', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemKindDefault', 'Special', { bg = 'NONE', default = true })
+  highlight.inherit('CmpItemMenuDefault', 'Pmenu', { bg = 'NONE', default = true })
   for name in pairs(types.lsp.CompletionItemKind) do
     if type(name) == 'string' then
-      vim.api.nvim_set_hl(0, ('CmpItemKind%sDefault'):format(name), { link = 'CmpItemKind', default = false })
+      vim.api.nvim_set_hl(0, ('CmpItemKind%sDefault'):format(name), { link = 'CmpItemKind', default = true })
     end
   end
 end)