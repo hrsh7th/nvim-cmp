159f9567ab91f462a00dc80f8caac4e5a3f5c2fc
f3fora <33097027+f3fora@users.noreply.github.com>
2022-10-04 17:41:25 +0200

    refactor(context): in...capture use vim.treesitter
    Use vim.treesitter.get_captures_at_cursor()

--- lua/cmp/config/context.lua
+++ lua/cmp/config/context.lua
@@ -21,57 +21,21 @@ end
 ---@param capture string | []string
 ---@return boolean
 context.in_treesitter_capture = function(capture)
-  local highlighter = require('vim.treesitter.highlighter')
-  local ts_utils = require('nvim-treesitter.ts_utils')
-  local buf = vim.api.nvim_get_current_buf()
+  local captures_at_cursor = require('vim.treesitter').get_captures_at_cursor()
 
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  row = row - 1
-  if vim.api.nvim_get_mode().mode == 'i' then
-    col = col - 1
-  end
-
-  local self = highlighter.active[buf]
-  if not self then
+  if vim.tbl_isempty(captures_at_cursor) then
     return false
-  end
-
-  local match = false
-
-  self.tree:for_each_tree(function(tstree, tree)
-    if not tstree then
-      return
-    end
-
-    local root = tstree:root()
-    local root_start_row, _, root_end_row, _ = root:range()
-    if root_start_row > row or root_end_row < row then
-      return
-    end
-
-    local query = self:get_query(tree:lang())
-    if not query:query() then
-      return
-    end
-
-    local iter = query:query():iter_captures(root, self.bufnr, row, row + 1)
-    for the_capture, node, _ in iter do
-      local hl = query.hl_cache[the_capture]
-
-      if hl and ts_utils.is_in_node_range(node, row, col) then
-        local c = query._query.captures[the_capture] -- name of the capture in the query
-        if type(capture) == 'string' and c == capture then
-          match = true
-          return
-        elseif type(capture) == 'table' and vim.tbl_contains(capture, c) then
-          match = true
-          return
-        end
+  elseif type(capture) == 'string' and vim.tbl_contains(captures_at_cursor, capture) then
+    return true
+  elseif type(capture) == 'table' then
+    for _, v in ipairs(capture) do
+      if vim.tbl_contains(captures_at_cursor, v) then
+        return true
       end
     end
-  end, true)
+  end
 
-  return match
+  return false
 end
 
 return context
