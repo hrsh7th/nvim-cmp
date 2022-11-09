f3be2c46f451b57e8bbe2acbd0506100a31c69bc
uga-rosa <uga6603@gmail.com>
2022-11-10 03:13:25 +0900

    fix(custom_entries_view): offset is also the number of bytes

--- lua/cmp/view/custom_entries_view.lua
+++ lua/cmp/view/custom_entries_view.lua
@@ -153,9 +153,8 @@ custom_entries_view.open = function(self, offset, entries)
   height = math.min(height, #self.entries)
 
   local pos = api.get_screen_cursor()
-  local cursor = api.get_cursor()
   local cursor_before_line = api.get_cursor_before_line()
-  local delta = vim.fn.strdisplaywidth(cursor_before_line:sub(-cursor[2])) + 1 - self.offset
+  local delta = vim.fn.strdisplaywidth(cursor_before_line:sub(self.offset))
   local row, col = pos[1], pos[2] - delta - 1
 
   local border_info = window.get_border_info({ style = completion })
