8a9e8a89eec87f86b6245d77f313a040a94081c1
pure-bliss <83361786+pure-bliss@users.noreply.github.com>
2022-11-18 09:31:06 +0100

    feat: added scrollbar option to window.completition ( #1087) (#1308)
    * #1087 added hide_scrollbar option to window.completition
    
    * Renamed hide_scrollbar to scrollbar, changed default value (#1087)
    
    * doc description update (#1087)
    
    * determining info.scrollbar value considering config scrollbar (#1087)
    
    * window.bordered scrollbar default value fix (#1087)

--- lua/cmp/types/cmp.lua
+++ lua/cmp/types/cmp.lua
@@ -117,6 +117,7 @@ cmp.ItemField = {
 ---@field public max_width integer|nil
 ---@field public max_height integer|nil
 ---@field public scrolloff integer|nil
+---@field public scrollbar boolean|true
 
 ---@class cmp.ConfirmationConfig
 ---@field public default_behavior cmp.ConfirmBehavior
