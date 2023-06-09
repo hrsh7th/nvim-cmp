0d702bef5831e589a87b5decb37b81afc7d55e4f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-09 11:59:36 +0200

    fix: don't use coroutine.isyieldable. Fixes #1607

--- lua/cmp/utils/async.lua
+++ lua/cmp/utils/async.lua
@@ -288,7 +288,7 @@ end
 
 -- This will yield when called from a coroutine
 function async.yield(...)
-  if not coroutine.isyieldable() then
+  if coroutine.running() == nil then
     error('Trying to yield from a non-yieldable context')
     return ...
   end
