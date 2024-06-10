fa570fed6cfbe4414415ed31ca801008170d86e6
xzb <xzb@MacBook-Pro.local>
2024-06-10 10:27:26 +0800

    revert

--- lua/cmp/view.lua
+++ lua/cmp/view.lua
@@ -296,7 +296,7 @@ view.on_entry_change = async.throttle(function(self)
     self.docs_view:close()
   end
 
-  e = e or self:get_selected_entry()
+  e = e or self:get_first_entry()
   if e then
     self.ghost_text_view:show(e)
   else
