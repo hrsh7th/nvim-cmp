fe5e98076ef7155109c3790b8b79fe3b0b29fef7
dmun <davidru01@live.nl>
2024-06-30 21:03:56 +0200

    feat: improve table field naming

--- lua/cmp/view/custom_entries_view.lua
+++ lua/cmp/view/custom_entries_view.lua
@@ -73,10 +73,11 @@ custom_entries_view.new = function()
 
             if type(v[field].hl_group) == 'table' then
               for _, extmark in ipairs(v[field].hl_group) do
-                vim.api.nvim_buf_set_extmark(buf, custom_entries_view.ns, i, o + extmark[1], {
+                local hl_start, hl_end = unpack(extmark.range)
+                vim.api.nvim_buf_set_extmark(buf, custom_entries_view.ns, i, o + hl_start, {
                   end_line = i,
-                  end_col = o + extmark[2],
-                  hl_group = extmark[3],
+                  end_col = o + hl_end,
+                  hl_group = extmark[1],
                   hl_eol = false,
                   ephemeral = true,
                 })
