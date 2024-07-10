a006da8b447ef893368918790f1809a89cab5348
tris203 <admin@snappeh.com>
2024-07-10 23:10:16 +0100

    fix(feedkeys): resolve issue with some copilot completions

--- lua/cmp/utils/feedkeys.lua
+++ lua/cmp/utils/feedkeys.lua
@@ -15,7 +15,7 @@ feedkeys.call = setmetatable({
       table.insert(queue, { keymap.t('<Cmd>setlocal lazyredraw<CR>'), 'n' })
       table.insert(queue, { keymap.t('<Cmd>setlocal textwidth=0<CR>'), 'n' })
       table.insert(queue, { keymap.t('<Cmd>setlocal backspace=2<CR>'), 'n' })
-      table.insert(queue, { keys, string.gsub(mode, '[itx]', ''), true })
+      -- table.insert(queue, { keys, string.gsub(mode, '[itx]', ''), true })
       table.insert(queue, { keymap.t('<Cmd>setlocal %slazyredraw<CR>'):format(vim.o.lazyredraw and '' or 'no'), 'n' })
       table.insert(queue, { keymap.t('<Cmd>setlocal textwidth=%s<CR>'):format(vim.bo.textwidth or 0), 'n' })
       table.insert(queue, { keymap.t('<Cmd>setlocal backspace=%s<CR>'):format(vim.go.backspace or 2), 'n' })
@@ -44,7 +44,7 @@ feedkeys.call = setmetatable({
 })
 feedkeys.run = function(id)
   if feedkeys.call.callbacks[id] then
-    feedkeys.call.callbacks[id]()
+    pcall(feedkeys.call.callbacks[id])
     feedkeys.call.callbacks[id] = nil
   end
   return ''
