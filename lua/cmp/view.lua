dde6d39accc8a0f1ae462a94207fdb23c3b14e24
toad <qq530901331@outlook.com>
2024-08-21 12:15:58 +0800

    fix: The items within the view of `Completion` are in the wrong order.

--- lua/cmp/view.lua
+++ lua/cmp/view.lua
@@ -81,6 +81,7 @@ view.open = function(self, ctx, sources)
     end
 
     -- create filtered entries.
+    local max_view_entries = config.get().performance.max_view_entries or 200
     local offset = ctx.cursor.col
     local group_entries = {}
     local max_item_counts = {}
@@ -96,8 +97,9 @@ view.open = function(self, ctx, sources)
           -- source order priority bonus.
           local priority = s:get_source_config().priority or ((#source_group - (i - 1)) * config.get().sorting.priority_weight)
 
-          for _, e in ipairs(s:get_entries(ctx)) do
-            e.score = e.score + priority
+	      local es =s:get_entries(ctx)
+          for index, e in ipairs(es) do
+            e.score = (e.score + priority) * max_view_entries + #es - index
             table.insert(group_entries, e)
             offset = math.min(offset, e:get_offset())
           end
@@ -128,7 +130,6 @@ view.open = function(self, ctx, sources)
       end
     end
 
-    local max_view_entries = config.get().performance.max_view_entries or 200
     entries = vim.list_slice(entries, 1, max_view_entries)
 
     -- open
