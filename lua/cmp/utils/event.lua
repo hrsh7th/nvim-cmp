b4c0e413fa92431c682764f22fe2a168d7c115d8
IguanaCucumber <sacha.fazzari@protonmail.com>
2024-09-13 14:37:23 +0000

    fix(utils): Do not call callback if type(callback) == "table"

--- lua/cmp/utils/event.lua
+++ lua/cmp/utils/event.lua
@@ -44,7 +44,9 @@ end
 ---@param name string
 event.emit = function(self, name, ...)
   for _, callback in ipairs(self.events[name] or {}) do
-    callback(...)
+    if not type(callback) == "table" then
+      callback(...)
+    end
   end
 end
 
