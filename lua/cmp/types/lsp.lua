c183f45faba8a80a7c58219a3600dc3f9e5a53e9
hrsh7th <hrsh7th@users.noreply.github.com>
2024-10-20 04:28:33 +0000

    Format with stylua

--- lua/cmp/types/lsp.lua
+++ lua/cmp/types/lsp.lua
@@ -65,8 +65,7 @@ lsp.Position = {
       return position
     end
 
-    local ok, byteindex = pcall(vim.str_byteindex,
-      text, position.character, from_encoding == lsp.PositionEncodingKind.UTF16)
+    local ok, byteindex = pcall(vim.str_byteindex, text, position.character, from_encoding == lsp.PositionEncodingKind.UTF16)
     if not ok then
       return position
     end
