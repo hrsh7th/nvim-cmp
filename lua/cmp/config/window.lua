0b1bb258ecbf34da1495373a6e38437023655091
Aqothy <aq0335@gmail.com>
2025-04-01 17:18:05 -0400

    fix: Use winborder for window menu and fix scrollbar window

--- lua/cmp/config/window.lua
+++ lua/cmp/config/window.lua
@@ -13,4 +13,13 @@ window.bordered = function(opts)
   }
 end
 
+window.get_border = function()
+  -- On neovim 0.11+, use the vim.o.winborder option by default
+  local has_winborder, winborder = pcall(function() return vim.o.winborder end)
+  if has_winborder and winborder ~= '' then return winborder end
+
+  -- On lower versions return the default
+  return 'none'
+end
+
 return window
