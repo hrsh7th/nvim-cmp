059e89495b3ec09395262f16b1ad441a38081d04
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2025-04-01 17:39:39 +0900

    fix: ref-fix backward compatibility

--- lua/cmp/utils/misc.lua
+++ lua/cmp/utils/misc.lua
@@ -197,7 +197,7 @@ end
 ---@return integer
 misc.to_utfindex = function(text, vimindex)
   vimindex = vimindex or #text + 1
-  if vim.fn.has('nvim-0.11') then
+  if vim.fn.has('nvim-0.11') == 1 then
     return vim.str_utfindex(text, 'utf-16', math.max(0, math.min(vimindex - 1, #text)))
   end
   return vim.str_utfindex(text, math.max(0, math.min(vimindex - 1, #text)))
