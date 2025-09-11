18a0f0e09023b5864ecdf67c3ebaed1744456ab8
tostrstrudl <89180993+tostrstrudl@users.noreply.github.com>
2025-09-11 02:19:37 -0700

    Added 'icon' to @field public lists

--- lua/cmp/types/vim.lua
+++ lua/cmp/types/vim.lua
@@ -1,6 +1,7 @@
 ---@class vim.CompletedItem
 ---@field public word string
 ---@field public abbr string|nil
+---@field public icon string|nil
 ---@field public kind string|nil
 ---@field public menu string|nil
 ---@field public equal 1|nil
@@ -8,6 +9,7 @@
 ---@field public dup 1|nil
 ---@field public id any
 ---@field public abbr_hl_group string|table|nil
+---@field public icon_hl_group string|table|nil
 ---@field public kind_hl_group string|table|nil
 ---@field public menu_hl_group string|table|nil
 
