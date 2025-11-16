5124cdd05549b7dac75b1968cf5f63091bd84b6f
Lawrence Wu <sites+github@llwu.me>
2025-11-15 17:15:58 -0800

    feat: `config.view.entries.vertical_positioning = 'above'|'below'|'auto'` (#1701)
    * feat: config.view.entries.vertical_positioning = 'auto'|'above'|'below'
    
    'above' works best with vim.opt.scrolloff = 999.
    
    Fixes: # 495
    Helped-by: Thanatermesis <thanatermesis@gmail.com>
    
    * fix(view): `docs_view` should go `bottom_up` if `custom_entries_view` did
    
    ---------
    
    Co-authored-by: Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>

--- lua/cmp/view.lua
+++ lua/cmp/view.lua
@@ -192,7 +192,8 @@ view.open_docs = function(self)
       if not self:visible() then
         return
       end
-      self.docs_view:open(e, self:_get_entries_view():info())
+      local bottom_up = self.custom_entries_view.bottom_up
+      self.docs_view:open(e, self:_get_entries_view():info(), bottom_up)
     end)))
   end
 end
@@ -296,7 +297,8 @@ view.on_entry_change = async.throttle(function(self)
         return
       end
       if self.is_docs_view_pinned or config.get().view.docs.auto_open then
-        self.docs_view:open(e, self:_get_entries_view():info())
+        local bottom_up = self.custom_entries_view.bottom_up
+        self.docs_view:open(e, self:_get_entries_view():info(), bottom_up)
       end
     end)))
   else
